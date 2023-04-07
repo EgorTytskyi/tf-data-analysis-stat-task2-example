5ed33cdca76444a9f618e4decb8c62b88518fa0b
EgorTytskyi <129987284+EgorTytskyi@users.noreply.github.com>
2023-04-08 01:13:36 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,15 @@
 import pandas as pd
 import numpy as np
-import scipy.stats as stats 
+import scipy.stats as sc
 
 
 chat_id = 628156322 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    mean = np.mean(x)
-    std = np.sqrt(2)
-    df = len(x) - 1
-    t_value = stats.t.ppf((1 + p) / 2, df)
-    se = std / np.sqrt(len(x))
-    lower_bound = mean - t_value * se
-    upper_bound = mean + t_value * se
-    return (lower_bound, upper_bound)
+    n = len(x)
+    t = 29
+    x = (-1)*x
+    scale = 0.5 + x.min()
+    D = 2.0/t/t
+    return D *(sc.laplace.ppf(0.5-p/2)/n - scale), \
+           D*(sc.laplace.ppf(0.5+p/2)/n - scale)
